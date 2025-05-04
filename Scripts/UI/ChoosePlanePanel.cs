edcbf2f16ef49a708004d1afef15c887ccb15f28
WinlanL <meno3013629683@163.com>
2025-05-04 18:26:14 +0800

    飞机切换界面优化

--- Scripts/UI/ChoosePlanePanel.cs
+++ Scripts/UI/ChoosePlanePanel.cs
@@ -6,9 +6,17 @@ using UnityEngine.UI;
 public class ChoosePlanePanel : MonoBehaviour
 {
     public Button StartGameBtn;
+    public Button leftBtn;
+    public Button rightBtn;
+
+    public GameObject[] players;
+    private int showIndex = 0;
     void Start()
     {
         StartGameBtn.onClick.AddListener(StartGameBtnClick);
+
+        leftBtn.onClick.AddListener(leftBtnClick);
+        rightBtn.onClick.AddListener(rightBtnClick);
     }
 
     
@@ -19,5 +27,21 @@ public class ChoosePlanePanel : MonoBehaviour
     public void StartGameBtnClick()
     {
         GameManager.Instance.LoadScene_04_Battle01();
+    }
+    public void leftBtnClick()
+    {
+        //Debug.Log("leftBtnClick");
+        players[showIndex].SetActive(false);
+        showIndex--;
+        showIndex = showIndex < 0 ? players.Length - 1 : showIndex;
+        players[showIndex].SetActive(true);
+    }
+    public void rightBtnClick()
+    {
+        //Debug.Log("rightBtnClick");
+        players[showIndex].SetActive(false);
+        showIndex++;
+        showIndex = showIndex % players.Length;
+        players[showIndex].SetActive(true);
     }
 }
